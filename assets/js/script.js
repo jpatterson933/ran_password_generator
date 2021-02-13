12bd01f2a9a555dc57b8e6e19894fce4a8936bc7
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-13 12:02:13 -0800

    added styling for boxtitle, length instructions, length input, click next button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,21 @@
 var button = document.createElement("button");
 var mainBox = document.createElement("box");
 var boxTitle = document.createElement("h1");
+var lengthTitle = document.createElement("p");
 var passwordLength = document.createElement("input")
 button.innerHTML = "Click to Start";
 boxTitle.innerHTML = "Choose Password Length";
+lengthTitle.innerHTML = "Password can be between 8 and 128 characters"
 
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
 body.appendChild(boxTitle);
+body.appendChild(lengthTitle);
 
 
 //BEG START BUTTON STYLE
 button.style.color = "white"; 
-button.style.backgroundColor = "rgb(173,216,230)";
+button.style.backgroundColor = "rgb(99, 150, 225)";
 button.style.border = "1px solid black";
 button.style.display = "flex";
 button.style.justifyContent = "center";
@@ -24,10 +27,14 @@ button.style.fontSize = "2em";
 //END START BUTTON STYLE
 
 boxTitle.style.display = "none";
+lengthTitle.style.display = "none";
 
 //when the button is clicked it will go away
 button.onclick = function () {
+    
+    //START BUTTON DISAPPEARS
     button.style.display = "none";
+
     //boxTitle Display STYLING - will be used to guide user through random password generation process
     boxTitle.style.display = "flex";
     boxTitle.style.justifyContent = "center";
@@ -37,6 +44,33 @@ button.onclick = function () {
     boxTitle.style.border = "1px black solid";
     boxTitle.style.borderRadius = "5px";
     //END OF BOX TITLE STYLING
+    
+    //this is displaying the input box for the user
+    document.getElementById("lengthInput").style.display = "flex"; 
+    document.getElementById("lengthInput").style.margin = "0 auto";
+
+    //BEG LENGTH TITLE STYLING
+    lengthTitle.style.display = "flex";
+    lengthTitle.style.justifyContent = "center";
+    lengthTitle.style.margin = "2em 25em 1em 25em";
+    lengthTitle.style.borderRadius = "5px";
+    lengthTitle.style.fontStyle = "bold";
+    lengthTitle.style.color = "rgb(99, 150, 225)"
+    //END LENGTH TITLE STYLING
+    
+    //BEG ENTER BUTTON STYLING
+    document.getElementById("pressEnter").style.display = "flex";
+    document.getElementById("pressEnter").style.justify = "center";
+    document.getElementById("pressEnter").style.margin = "1em auto 0 auto";
+    document.getElementById("pressEnter").style.backgroundColor = "rgb(99, 150, 225)";
+    document.getElementById("pressEnter").style.padding = ".5em";
+    document.getElementById("pressEnter").style.color = "white";
+    document.getElementById("pressEnter").style.border = "1px solid black";
+    document.getElementById("pressEnter").style.borderRadius = "5px";
+    //END ENTER BUTTON STYLING
+
+    
 }
+
 
 

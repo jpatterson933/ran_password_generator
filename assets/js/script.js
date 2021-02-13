a1e5b69336a535ea565b0deae0d5d72191977d4d
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-13 11:11:43 -0800

    added box title for program, added full styling for box title

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 
-
 var button = document.createElement("button");
-
+var mainBox = document.createElement("box");
+var boxTitle = document.createElement("h1");
+var passwordLength = document.createElement("input")
 button.innerHTML = "Click to Start";
+boxTitle.innerHTML = "Choose Password Length";
 
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
+body.appendChild(boxTitle);
 
-button.addEventListener ("click", function() {
-  alert("did something");
-});
 
 //BEG START BUTTON STYLE
 button.style.color = "white"; 
@@ -22,3 +22,21 @@ button.style.padding = "1em 2em 1em 2em";
 button.style.borderRadius = "5px";
 button.style.fontSize = "2em";
 //END START BUTTON STYLE
+
+boxTitle.style.display = "none";
+
+//when the button is clicked it will go away
+button.onclick = function () {
+    button.style.display = "none";
+    //boxTitle Display STYLING - will be used to guide user through random password generation process
+    boxTitle.style.display = "flex";
+    boxTitle.style.justifyContent = "center";
+    boxTitle.style.margin = "2em 10em 0em 10em";
+    boxTitle.style.padding = "1em 1em 1em 1em";
+    boxTitle.style.color = "rgb(99, 150, 225)";
+    boxTitle.style.border = "1px black solid";
+    boxTitle.style.borderRadius = "5px";
+    //END OF BOX TITLE STYLING
+}
+
+

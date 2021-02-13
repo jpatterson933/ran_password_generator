cd82e09973fb144e3ce259a0eea0e630631e4033
Jeffery Patterson <jpatterson933@ucla.edu>
2021-02-13 10:44:36 -0800

    finalized start button style

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,14 @@ button.addEventListener ("click", function() {
   alert("did something");
 });
 
-//start program button style
+//BEG START BUTTON STYLE
 button.style.color = "white"; 
 button.style.backgroundColor = "rgb(173,216,230)";
 button.style.border = "1px solid black";
-button.style.display = "none";
+button.style.display = "flex";
+button.style.justifyContent = "center";
+button.style.margin = "4em auto 0 auto";
+button.style.padding = "1em 2em 1em 2em";
+button.style.borderRadius = "5px";
+button.style.fontSize = "2em";
+//END START BUTTON STYLE
